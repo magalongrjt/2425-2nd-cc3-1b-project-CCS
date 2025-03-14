392953624f284b3e1357b6e0339168d918373ef5
magalongrjt <rtm2821@students.uc-bcf.edu.ph>
2025-03-14 08:18:41 +0800

    Create User.java
    Signed-off-by: magalongrjt <rtm2821@students.uc-bcf.edu.ph>

---
+++ Codes/User.java
@@ -0,0 +1,19 @@
+class User {
+    private String userID;
+    private String userEmail;
+    private String password;
+
+    public User(String userID, String userEmail, String password) {
+        this.userID = userID;
+        this.userEmail = userEmail;
+        this.password = password;
+    }
+
+    public void login() {
+        System.out.println(userEmail + " logged in.");
+    }
+
+    public void logout() {
+        System.out.println(userEmail + " logged out.");
+    }
+}
