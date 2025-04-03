92910071d7d39e496087f76017b517cd2132d592
magalongrjt <rtm2821@students.uc-bcf.edu.ph>
2025-04-03 20:44:18 +0800

    Update GUI.java
    Signed-off-by: magalongrjt <rtm2821@students.uc-bcf.edu.ph>

--- src/GUI.java
+++ src/GUI.java
@@ -3,85 +3,77 @@
  *      add functionality to login 
  */
 import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI extends JFrame implements ActionListener{
+public class GUI extends JFrame implements ActionListener {
 
     JButton loginButton;
     JTextField userName;
     JTextField password;
     JLabel message;
-    GUI(){
-        
-        //TEMP CODE
+
+    GUI() {
+
+        // TEMP CODE
         message = new JLabel();
         message.setText("login feature to be implemented");
         message.setVisible(false);
-        
-        //title
-        JLabel title = new JLabel();//create label
-        title.setText("Employee Management System");//set text for label
-        
-        //Credentials text felids
-        userName = new JTextField();
-        userName.setPreferredSize(new Dimension(300,40));
-        password = new JTextField();
-        password.setPreferredSize(new Dimension(300,40));
 
-        //login button
-        loginButton = new JButton();
+        // Title
+        JLabel title = new JLabel();
+        title.setText("Employee Management System");
+
+        // Credentials fields
+        userName = new JTextField();
+        userName.setPreferredSize(new Dimension(300, 40));
+        password = new JTextField();
+        password.setPreferredSize(new Dimension(300, 40));
+
+        // Login button
+        loginButton = new JButton("Login");
         loginButton.addActionListener(this);
-        loginButton.setText("login");
         loginButton.setFocusable(false);
-        
-        //Creates title panel
+
+        // Title panel
         JPanel titlePanel = new JPanel();
-        titlePanel.setPreferredSize(new Dimension(2000, 30)); //TODO: make width responsive (react when window is resized)
+        titlePanel.setPreferredSize(new Dimension(800, 30)); 
         titlePanel.setBackground(Color.GRAY);
         titlePanel.add(title);
-        
-        //Creates login panel
+
+        // Login panel
         JPanel loginPanel = new JPanel();
-        loginPanel.setPreferredSize(new Dimension (300,200)); 
-        loginPanel.add(new JLabel("username"));
+        loginPanel.setPreferredSize(new Dimension(300, 200));
+        loginPanel.add(new JLabel("Username"));
         loginPanel.add(userName);
-        loginPanel.add(new JLabel("password"));
+        loginPanel.add(new JLabel("Password"));
         loginPanel.add(password);
         loginPanel.add(loginButton);
-        
-        //Creates the window
+
+        // Window settings
         this.setTitle("CCS Employee Management System");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 600);
-        this.setResizable(true);
         this.setLayout(new FlowLayout());
         this.add(titlePanel);
         this.add(loginPanel);
-        this.add(message);//TEMP CODE
+        this.add(message);
         this.setVisible(true);
     }
-    
+
     @Override
-    public void actionPerformed(ActionEvent e){
-        /* TODO: disable button if input field for login credentials is empty
-        *  loginButton.setEnabled(false);
-        */
-        if(e.getSource()==loginButton){
-            //TEMP CODE
-            message.setVisible(true);
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginButton) {
+            // Close login screen
+            this.dispose();
+            
+            // Open TeamsMenuGUI
+            new TeamsMenuGUI();
         }
     }
-        
-public class Main {
+
     public static void main(String[] args) {
         new GUI();
     }
-}
-
-        
 }
